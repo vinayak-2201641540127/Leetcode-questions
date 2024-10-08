eac34582c2b0caebd10aeb446ad25aed67054ef5
Vinayak Shukla <youremail@example.com>
2024-10-08 11:53:06 +0530

    Second

---
+++ 1963.minimum-number-of-swaps-to-make-the-string-balanced.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=1963 lang=java
+ *
+ * [1963] Minimum Number of Swaps to Make the String Balanced
+ */
+
+// @lc code=start
+class Solution {
+    public int minSwaps(String s) {
+        
+    }
+}
+// @lc code=end
+
